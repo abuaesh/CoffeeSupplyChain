794ff20b7464d75d548763a15857e15b780b02fc
abuaesh <52622393+abuaesh@users.noreply.github.com>
2020-02-05 00:11:51 +0300

    feat: Add functions and modifiers
    type: feat

--- project-6/contracts/coffeeaccesscontrol/DistributorRole.sol
+++ project-6/contracts/coffeeaccesscontrol/DistributorRole.sol
@@ -7,42 +7,44 @@ import "./Roles.sol";
 contract DistributorRole {
 
   // Define 2 events, one for Adding, and other for Removing
+  event DistributorAdded(address _consumer);
+  event DistributorRemoved(address _consumer);
 
   // Define a struct 'distributors' by inheriting from 'Roles' library, struct Role
-
+  Role distributors;
   // In the constructor make the address that deploys this contract the 1st distributor
   constructor() public {
-
+    _add(msg.sender);
   }
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
   modifier onlyDistributor() {
-
+    require(has(distibutors, msg.sender));
     _;
   }
 
   // Define a function 'isDistributor' to check this role
   function isDistributor(address account) public view returns (bool) {
-
+    return has(distributors, account);
   }
 
   // Define a function 'addDistributor' that adds this role
   function addDistributor(address account) public onlyDistributor {
-
+    _addDistributor(account);
   }
 
   // Define a function 'renounceDistributor' to renounce this role
   function renounceDistributor() public {
-
+    _removeDistributor(msg.sender);
   }
 
   // Define an internal function '_addDistributor' to add this role, called by 'addDistributor'
   function _addDistributor(address account) internal {
-
+    add(distributors, account);
   }
 
   // Define an internal function '_removeDistributor' to remove this role, called by 'removeDistributor'
   function _removeDistributor(address account) internal {
-
+    remove(distributor, account);
   }
 }
