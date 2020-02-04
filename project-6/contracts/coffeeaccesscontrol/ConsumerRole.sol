3d5c6fb82f59323e5740e7313795ef853f08919f
abuaesh <52622393+abuaesh@users.noreply.github.com>
2020-02-05 00:07:52 +0300

    feat: Update consumers struct
    type: feat

--- project-6/contracts/coffeeaccesscontrol/ConsumerRole.sol
+++ project-6/contracts/coffeeaccesscontrol/ConsumerRole.sol
@@ -6,36 +6,32 @@ import "./Roles.sol";
 // Define a contract 'ConsumerRole' to manage this role - add, remove, check
 contract ConsumerRole {
 
-  Consumers consumerList;
-
   // Define 2 events, one for Adding, and other for Removing
   event ConsumerAdded(address _consumer);
   event ConsumerRemoved(address _consumer);
 
   // Define a struct 'consumers' by inheriting from 'Roles' library, struct Role
-  struct Consumers is Role{
-  
-  }
+ Role consumers;
 
   // In the constructor make the address that deploys this contract the 1st consumer
   constructor() public {
-      add(consumerList, msg.sender);
+     _add(msg.sender);
   }
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
   modifier onlyConsumer() {
-    require(has(consumerList, msg.sender));
+    require(has(consumers, msg.sender));
     _;
   }
 
   // Define a function 'isConsumer' to check this role
   function isConsumer(address account) public view returns (bool) {
-    return(has(consumerList, account));
+    return(has(consumers, account));
   }
 
   // Define a function 'addConsumer' that adds this role
   function addConsumer(address account) public onlyConsumer {
-    addConsumer(account);
+    _addConsumer(account);
   }
 
   // Define a function 'renounceConsumer' to renounce this role
@@ -45,11 +41,11 @@ contract ConsumerRole {
 
   // Define an internal function '_addConsumer' to add this role, called by 'addConsumer'
   function _addConsumer(address account) internal {
-    add(consumerList, account);
+    add(consumers, account);
   }
 
   // Define an internal function '_removeConsumer' to remove this role, called by 'removeConsumer'
   function _removeConsumer(address account) internal {
-    remove(consumerList, msg.sender);    
+    remove(consumers, msg.sender);    
   }
 }
