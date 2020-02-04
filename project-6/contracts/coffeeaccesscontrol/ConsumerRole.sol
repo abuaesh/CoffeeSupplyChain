08299db66b3c32d4a5628df5fd420cc54be44a81
abuaesh <52622393+abuaesh@users.noreply.github.com>
2020-02-05 00:19:33 +0300

    feat: Emit appropriate events
    type: feat
    Emit appropriate events for the _addConsumer and _removeConsumer functions

--- project-6/contracts/coffeeaccesscontrol/ConsumerRole.sol
+++ project-6/contracts/coffeeaccesscontrol/ConsumerRole.sol
@@ -42,10 +42,12 @@ contract ConsumerRole {
   // Define an internal function '_addConsumer' to add this role, called by 'addConsumer'
   function _addConsumer(address account) internal {
     add(consumers, account);
+    emit ConsumerAdded(account);
   }
 
   // Define an internal function '_removeConsumer' to remove this role, called by 'removeConsumer'
   function _removeConsumer(address account) internal {
     remove(consumers, msg.sender);    
+    emit ConsumerRemoved(account);
   }
 }
